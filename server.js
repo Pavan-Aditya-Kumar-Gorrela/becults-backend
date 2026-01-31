65e5af679f3c5403b2bc8b279de0c3808ba9f3a5
Pavan-Aditya-Kumar-Gorrela <kumarpavanaditya@gmail.com>
2026-01-31 21:31:29 +0530

    server modified

--- server.js
+++ server.js
@@ -59,6 +59,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-  console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
 
 });
