c072963d75991fda33e3b7fafca4fd0c63ddd0cf
Pavan-Aditya-Kumar-Gorrela <kumarpavanaditya@gmail.com>
2026-02-19 18:07:01 +0530

    minfix

--- services/mailerService.js
+++ services/mailerService.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_PORT === '465', // true for 465, false for other ports
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
